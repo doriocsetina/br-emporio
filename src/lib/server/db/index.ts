7c0ae146e816c00a8f7d439219282056ad5b0bd0
ari <arimui@proton.me>
2025-09-27 23:05:40 +0200

    infra: added debug log line

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -4,7 +4,7 @@ import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
+console.log(env.DATABASE_URL)
 const client = new Database(env.DATABASE_URL);
 
 export const db = drizzle(client, { schema });
